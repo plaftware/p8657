898814e1e0de196389c6be6a9eb163ff79288d14
Freddy Barrios <freddybarrios.077@gmail.com>
2017-01-13 16:36:23 -0500

    commit

--- app/src/main/java/com/asdf123/as3f/log/MyLog.java
+++ app/src/main/java/com/asdf123/as3f/log/MyLog.java
@@ -2,46 +2,51 @@ package com.asdf123.as3f.log;
 
 import android.util.Log;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+
 /**
  * Created by l2 on 9/01/17.
  */
 
 public class MyLog {
 
-    protected static StringBuffer log = new StringBuffer();
-    protected static final int MAX_LENGTH = 20480; // 20k text
+    protected static LinkedList<String> log = new LinkedList<>();
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh:mm a");
 
     public static void d(String tag, String txt) {
         Log.d(tag, txt);
-        append(txt + "<br>");
+        log.add("[ " + simpleDateFormat.format(new Date()) + " ]" + " " +txt);
     }
 
     public static void e(String tag, String txt) {
         Log.e(tag, txt);
-        append("<font color='#c57731'>" + txt + "</font><br>");
+        log.add("[ " + simpleDateFormat.format(new Date()) + " ]" + " " +txt);
     }
 
     public static void e(String tag, String txt, Exception e) {
         Log.e(tag, txt,e);
-        append("<font color='#c57731'>" + txt + "<br>Exception - " + e.toString() + "</font><br>");
+        log.add("[ " + simpleDateFormat.format(new Date()) + " ]" + " " +txt);
+        log.add("[ " + simpleDateFormat.format(new Date()) + " ]" + " " +"|Exception - " + e.toString());
     }
 
     public static void i(String tag, String txt) {
         Log.i(tag, txt);
-        append("<i>" + txt + "</i><br>");
+        log.add("[ " + simpleDateFormat.format(new Date()) + " ]" + " " +txt);
     }
 
-    public static synchronized void append(String s) {
+/*    public static synchronized void append(String s) {
         s = s.replace("com.asdf123", "");
         s = s.replace("as3f", "anonymousVPN");
         if (log.length() + s.length() > MAX_LENGTH) {
             log.delete(0, log.length() + s.length() - MAX_LENGTH);
         }
         log.append(s, Math.max(0, s.length() - MAX_LENGTH), s.length());
-    }
+    }*/
 
-    public static String dump() {
-        return log.toString();
+    public static LinkedList<String> dump() {
+        return log;
     }
 
 }
